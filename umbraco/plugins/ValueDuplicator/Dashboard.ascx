a1aa6ff3b27e40474af5809eb34ab144427a6585
Marc <marc.stoecker@mindrevolution.com>
2013-07-04 21:53:57 +0200

    module loading and embedding.

---
+++ umbraco/plugins/ValueDuplicator/Dashboard.ascx
@@ -0,0 +1,54 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Dashboard.ascx.cs" Inherits="ValueDuplicator.Dashboard" %>
+
+<h1>Copy node data values</h1>
+
+<asp:DropDownList ID="DoctypesList" runat="server" AutoPostBack="true" onselectedindexchanged="DoctypesList_Changed" CssClass="bigInput"></asp:DropDownList>
+
+<asp:PlaceHolder ID="FieldsPanel" runat="server" Visible="false">
+    <div class="propertypane" style="margin-top:20px;">
+        <div style="width:50%;float:left;">
+            <h3>Source: <asp:DropDownList ID="PropertiesList" runat="server" AutoPostBack="true" onselectedindexchanged="PropertiesList_Changed"></asp:DropDownList></h3>
+            <asp:PlaceHolder ID="PropertyInfo" runat="server" Visible="false">
+                <p>
+                    Alias: <asp:Literal ID="piAlias" runat="server"></asp:Literal>
+                    <br />
+                    Data Type: <asp:Literal ID="piType" runat="server"></asp:Literal>
+                </p>
+            </asp:PlaceHolder>
+        </div>
+        <div style="width:40%;float:left;">
+            <asp:PlaceHolder ID="TargetPropertyPanel" runat="server">
+                <div>
+                    <h3>Target: <asp:DropDownList ID="TargetProperty" runat="server" AutoPostBack="true" onselectedindexchanged="TargetProperty_Changed"></asp:DropDownList></h3>
+                    <asp:PlaceHolder ID="TargetPropertyInfo" runat="server" Visible="false">
+                        <p>
+                            Alias: <asp:Literal ID="tpAlias" runat="server"></asp:Literal>
+                            <br />
+                            Data Type: <asp:Literal ID="tpType" runat="server"></asp:Literal>
+                        </p>
+                    </asp:PlaceHolder>
+                </div>
+            </asp:PlaceHolder>
+        </div>
+        <div style="clear:both;"></div>
+    </div>
+
+    <asp:PlaceHolder ID="DifferentDatatypeWarning" runat="server" Visible="false">
+        <p style="color:red;">
+            Please make sure that the copied values will be compatible with the target data type.
+        </p>
+    </asp:PlaceHolder>
+
+</asp:PlaceHolder>
+
+<asp:PlaceHolder ID="CopyProcess" runat="server" Visible="false">
+
+    <div class="propertypane" style="margin-top:20px;">
+        <p>
+            <strong><asp:Button ID="StartCopy" runat="server" Text="Copy" CssClass="bigInput" style="width:auto;padding:0px 10px;" OnClick="StartCopy_Click" /> value to for all documents of this type to the target.</strong>
+        </p>
+        <div ID="CopiedNodes" runat="server">
+        </div>
+    </div>
+
+</asp:PlaceHolder>
---
+++ umbraco/plugins/ValueDuplicator/Dashboard.ascx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using umbraco.cms.businesslogic.web;
+using umbraco.cms.businesslogic.propertytype;
+
+namespace ValueDuplicator
+{
+    public partial class Dashboard : System.Web.UI.UserControl
+    {
+        private ListItem chooseItemFromList = new ListItem("Choose ...", string.Empty);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                DoctypesList.Items.Add(chooseItemFromList);
+                foreach (DocumentType doctype in umbraco.cms.businesslogic.web.DocumentType.GetAllAsList())
+                {
+                    DoctypesList.Items.Add(new ListItem(doctype.Text + " (" + doctype.Alias + ")", doctype.Id.ToString()));
+                }
+            }
+        }
+
+        protected void DoctypesList_Changed(object sender, EventArgs e)
+        {
+            // - there is a doctype selected
+            if (!string.IsNullOrEmpty(DoctypesList.SelectedValue))
+            {
+                DocumentType doctype = new umbraco.cms.businesslogic.web.DocumentType(System.Convert.ToInt32(DoctypesList.SelectedValue));
+                if (doctype != null)
+                {
+                    PropertiesList.Items.Clear();
+                    PropertiesList.Items.Add(chooseItemFromList);
+                    foreach (PropertyType prop in doctype.PropertyTypes)
+                    {
+                        PropertiesList.Items.Add(new ListItem(prop.Name + " (" + prop.Alias + ")", prop.Id.ToString()));
+                    }
+                }
+
+                FieldsPanel.Visible = (doctype != null);
+                TargetPropertyPanel.Visible = !FieldsPanel.Visible;
+            }
+            else
+            {
+                FieldsPanel.Visible = false;
+            }
+        }
+
+        protected void PropertiesList_Changed(object sender, EventArgs e)
+        {
+            // - there is a property selected
+            if (!string.IsNullOrEmpty(PropertiesList.SelectedValue))
+            {
+                PropertyType prop = new PropertyType(Convert.ToInt32(PropertiesList.SelectedValue));
+
+                if (prop != null)
+                {
+                    // - property info
+                    piAlias.Text = prop.Alias;
+                    piType.Text = prop.DataTypeDefinition.DataType.DataTypeName;
+
+                    // - fill target property list
+                    DocumentType doctype = new umbraco.cms.businesslogic.web.DocumentType(System.Convert.ToInt32(DoctypesList.SelectedValue));
+                    if (doctype != null)
+                    {
+                        TargetProperty.Items.Clear();
+                        TargetProperty.Items.Add(chooseItemFromList);
+                        foreach (PropertyType pi in doctype.PropertyTypes)
+                        {
+                            // - don't add originating property!
+                            if (pi.Id != prop.Id)
+                                TargetProperty.Items.Add(new ListItem(pi.Name + " (" + pi.Alias + ")", pi.Id.ToString()));
+                        }
+                    }
+
+                    PropertyInfo.Visible = true;
+                    TargetPropertyPanel.Visible = true;
+                    TargetPropertyInfo.Visible = false;
+                }
+            }
+            else
+            {
+                TargetPropertyPanel.Visible = false;
+                TargetPropertyInfo.Visible = false;
+            }
+        }
+
+        protected void TargetProperty_Changed(object sender, EventArgs e)
+        {
+            // - there is a target property selected
+            if (!string.IsNullOrEmpty(TargetProperty.SelectedValue))
+            {
+                PropertyType prop = new PropertyType(Convert.ToInt32(PropertiesList.SelectedValue));
+                PropertyType targetprop = new PropertyType(Convert.ToInt32(TargetProperty.SelectedValue));
+
+                if (targetprop != null)
+                {
+                    // - property info
+                    tpAlias.Text = targetprop.Alias;
+                    tpType.Text = targetprop.DataTypeDefinition.DataType.DataTypeName;
+
+                    // - no of same type as source? show warning!
+                    DifferentDatatypeWarning.Visible = (prop.DataTypeDefinition.DataType.Id != targetprop.DataTypeDefinition.DataType.Id);
+
+                    TargetPropertyInfo.Visible = true;
+                    CopyProcess.Visible = true;
+                }
+            }
+            else
+            {
+                TargetPropertyInfo.Visible = false;
+            }
+
+        }
+
+        protected void StartCopy_Click(object sender, EventArgs e)
+        {
+            // - get all documents of this type
+            DocumentType doctype = new umbraco.cms.businesslogic.web.DocumentType(System.Convert.ToInt32(DoctypesList.SelectedValue));
+            umbraco.cms.businesslogic.Content[] docs = Document.getContentOfContentType(doctype);
+
+            Document doc;
+            foreach (umbraco.cms.businesslogic.Content d in docs)
+            {
+                doc = new Document(d.Id);
+                CopiedNodes.InnerText += doc.Text + "--" + doc.Id.ToString() + "--";
+            }
+        }
+    }
+}
